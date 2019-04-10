83268f64600dc0b093008ebf598267d2590404b1
marumaru1759 <marum1759@gmail.com>
2019-04-10 23:17:27 +0900

    many point is modified

--- src/components/CustomeWel.vue
+++ src/components/CustomeWel.vue
@@ -1,57 +1,79 @@
 <template>
 	<div>
-	<header class="nav">
-        <div class="nav-left">
-        	<span class="nav-item">
-        		<span class="fa fa-language"></span>Locasearch
-        	</span>
-        </div>
-        <div class="nav-right">
-        	<span class="nav-item">
-        		<router-link to="/login" class="fa fa-user">ログイン</router-link>
-			</span>
-		</div>
+	
+	<section class="hero is-info is-fullheight">
 
-		<section class="hero">
-			<div class="hero-body">
-				<div class="conatainer">
-					<h1 class="title">Locasearch</h1>
-					<h2 class="subtitle">あなたの行きたいを見つけるサイト</h2>
-				</div>
+	<header>
+		<div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <h1 class="navbar-item is-size-3">
+                            Locasearch
+                        </h1>
+
+                        <span class="navbar-burger burger" data-target="navbarMenu">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </div>
+
+                    <div id="navbarMenu" class="navbar-menu">
+                        <div class="navbar-start">
+                            <span class="navbar-item">
+                                <a class="is-white is-outlined" href="#">
+                                    Home
+                                </a>
+                            </span>
+                            <span class="navbar-item">
+                                <router-link to="/info" class="is-white  is-outlined">お知らせ</router-link>      
+                                
+                            </span>
+                            <span class="navbar-item">
+                                <router-link to="/users" class="is-white is-outlined">UsersList</router-link>
+                            </span>
+                         </div>
+                         <div class="navbar-end">
+                            <span class="navbar-item">
+                                <span>
+                                    <router-link to="/login" class="button is-info">Login</router-link>
+                                </span>
+                            </span>
+                         </div>
+                    </div>
+                </div>
+            </nav>
+            </div>
+         </header>
+
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<div class="column is-half is-offset-3">
+                        <h1 class="title">
+                            ロケサーチ（仮）
+                        </h1>
+                         <h2 class="subtitle">
+                            あなたの行きたいを見つけるサイト
+                        </h2>
+                        <router-link to="/register" class="button is-medium is-info">新規登録</router-link>
+
+                </div>   
 			</div>
-		</section>
-	</header>
-
-	<section class="level">
-		<div class="level-right">
-			<div class="level-item tabs">
-				<ul>
-					<li class"is-active">
-						 <router-link to="/info" class="nav-link">お知らせ</router-link></li>
-						 <li><router-link to="/mypage" class="nav-link">マイページ</router-link></li>
-						 <li><router-link to="/users" class="nav-link">UsersList</router-link></li>
-				</ul>
-			</div>
 		</div>
-	</section>
-
-	<main class="columns">
-		<div class="content">
-        	<h1>ロケサーチ（仮）</h1>
-        	<p>あなたの行きたいを見つけるサイト</p>
-			<router-link to="/register" class="button">新規登録</router-link>
-        </div>
-     </main>
 
 	<footer class="container">
         <div class="content">
           <nav class="nav justify-content-center">
-            <a class="nav-link" href="#">利用規約</a> 
-            <a class="nav-link" href="#">当サイトについて</a>
-            <a class="nav-link" href="#">お問い合わせ</a>
+            <a>利用規約</a> 
+            <a>当サイトについて</a>
+            <a>お問い合わせ</a>
           </nav>
         </div>
-     </footer>
+    </footer>
+	 
+	</section>
+ 
  </div>
 </template>
 
